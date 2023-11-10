5959980761ad0f07d9655416d0999042d53989a4
wungthing <wungthingkashungnao123@gmail.com>
2023-11-10 14:35:17 +0530

    best time to buy and sell stocks

---
+++ blind 75/2.Best time to buy and sell stocks/Main.java
@@ -0,0 +1,20 @@
+ class Main {
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[] {7,1,5,3,6,4} ));
+    }
+
+    
+    public static int maxProfit(int[] prices){
+        int min = prices[0];
+        int profit = 0;
+
+        for(int i = 0; i<prices.length;i++){
+            if(prices[i]<min){
+                min = prices[i];
+            }
+            profit = Math.max(profit, prices[i]-min);
+        }
+        return profit;
+    }
+}
