5614b0c1df62b45fa312db6be78255a21f8b5128
wungthing <wungthingkashungnao123@gmail.com>
2024-02-24 20:32:27 +0530

    two sum

---
+++ javascript Blind 75/1. Two Sum/twoSum.js
@@ -0,0 +1,12 @@
+let twoSum = (nums, target) => {
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let compliment = target - nums[i];
+    if (map.has(compliment)) {
+      return [i, map.get(compliment)];
+    } else {
+      map.set(nums[i], i);
+    }
+  }
+};
+console.log(twoSum([2, 7, 11, 15], 9));
